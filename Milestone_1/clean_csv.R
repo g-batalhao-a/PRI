30681734694f00246789477a117808b36f42022b
g-batlhao-a <up201806451@fe.up.pt>
2021-11-12 12:42:39 +0000

    Added URL column and started date modification

--- Milestone_1/clean_csv.R
+++ Milestone_1/clean_csv.R
@@ -1,6 +1,8 @@
-if (require("readr")) install.packages("readr")
-if (require("dplyr")) install.packages("dplyr")
-if (require("lubridate")) install.packages("lubridate")
+if (!require("readr")) install.packages("readr")
+if (!require("dplyr")) install.packages("dplyr")
+if (!require("lubridate")) install.packages("lubridate")
+if (!require("stringr")) install.packages("stringr")
+if (!require("tidyr")) install.packages("tidyr")
 
 recipes <- read_csv("data/recipes.csv")
 reviews <- read_csv("data/reviews.csv")
@@ -8,7 +10,6 @@ reviews <- read_csv("data/reviews.csv")
 # Copy of columns 
 author.recipes <- select(recipes, c('AuthorId', 'AuthorName'))
 author.reviews <- select(reviews, c('AuthorId', 'AuthorName'))
-recipes.images <- data.frame("Id"=integer(),"RecipeId"=integer(),"Image"=character(), stringsAsFactors = FALSE)
 
 # Remove AuthorName and Images
 new.recipes <- select(recipes, -c('AuthorName','Images'))
@@ -24,6 +25,15 @@ author.reviews <- distinct(author.reviews,.keep_all = TRUE)
 new.recipes$CookTime <- as.numeric(duration(new.recipes$CookTime))
 new.recipes$PrepTime <- as.numeric(duration(new.recipes$PrepTime))
 new.recipes$TotalTime <- as.numeric(duration(new.recipes$TotalTime))
+
+# Remove Hour from dates
+str_split(new.recipes$DatePublished, " ")[1]
+new.recipes <- separate(new.recipes, DatePublished, "DatePublished", sep = " ", extra = "drop")
+
+#Create Links
+new.recipes <- new.recipes %>% mutate(URL = paste("https://www.food.com/recipe/",
+                                                  tolower(str_replace_all(str_replace_all(Name,"&quot;","\""), c("[;,:.()%!\"#$%&/()=@{}\\[\\]'«»+*<>\\|]" = "", " +" = "-", "-+" = "-"))),
+                                                  "-", RecipeId, sep = ""))
 
 #Build user table
 users <- merge(author.recipes,author.reviews,by = "AuthorId",all =TRUE, sort = TRUE)
