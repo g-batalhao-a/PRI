517a8ae4b93e9ecd3b2e70dccbef864349894cdd
g-batalhao-a <al.darkseid.2010@gmail.com>
2021-11-14 20:33:00 +0000

    Add statistical analysis

--- Milestone_1/statistical_analysis.R
+++ Milestone_1/statistical_analysis.R
@@ -3,12 +3,19 @@ if (!require("lessR")) install.packages("lessR")
 
 #Recipe Analysis
 barplot(sort(table(new.recipes$RecipeCategory),decreasing = T))
-plot(table(new.recipes$Calories),xlab = "Calories",ylab = "Num of Recipes")
+plot(table(new.recipes$Calories),xlab = "Calories",ylab = "Num of Recipes", col=2,xaxt='n', main=paste("Calories Per Recipe"))
+#modify x-axis interval
+axis(side=1, at=seq(0, 4700, by=100))
+
 plot(table(new.recipes$PrepTime),xlab = "Total Time",ylab = "Num of Recipes")
 hist(as.numeric(format(as.Date(new.recipes$DatePublished, "%Y-%m-%d"),"%Y")), main = paste("Histogram of Recipes Per Year"),xlab = "Year",col =3)
+graph.recipes <- filter(new.recipes, ReviewCount < 20 & ReviewCount > 0)
+barplot(prop.table(table(graph.recipes$ReviewCount)), main = paste("Frequency of Reviews"), xlab = "Reviews", col = 7)
 
 #Reviews Analysis
-hist(new.reviews$Rating, main = paste("Histogram of Ratings"),xlab = "Rating", col = 4)
+barplot(prop.table(table(new.reviews$Rating)), main = paste("Frequency of Ratings"), xlab = "Rating", col = 4)
+hist(as.numeric(format(as.Date(new.reviews$DateModified, "%Y-%m-%d"),"%Y")), main = paste("Histogram of Reviews Per Year"),xlab = "Year",col =3)
+
 
 
 summary(new.recipes)
