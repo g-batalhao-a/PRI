d018d26d0a14b925c9e1571dc0409a8da93bbf53
pedrojfs17 <up201806227@fe.up.pt>
2021-12-08 14:04:20 +0000

    Updated schema and merge script

--- Milestone_2/merge_nest.py
+++ Milestone_2/merge_nest.py
@@ -1,5 +1,8 @@
+import numpy as np
 import pandas
 import simplejson
+import random
+import re
 
 ### Users
 print("Parsing Users")
@@ -10,33 +13,53 @@ users_data.rename({"Id": "AuthorId", "Name": "AuthorName"}, axis = 1, inplace = 
 print("Parsing Reviews")
 reviews_data = pandas.read_csv("data/solr/reviews.csv")
 reviews_data['Type'] = "review"
-reviews_data.rename({"Id": "ReviewId", "UserId": "AuthorId"}, axis = 1, inplace = True)
-reviews_data['DateModified'] = reviews_data['DateModified'].astype(str) + 'T00:00:00Z'
+reviews_data.rename({
+    "Id": "ReviewId", 
+    "UserId": "AuthorId",
+    "DateModified": "Date"
+}, axis = 1, inplace = True)
+reviews_data['Date'] = reviews_data['Date'].astype(str) + 'T00:00:00Z'
 reviews_data = reviews_data.merge(users_data, how='left', on='AuthorId')
 
 ### Recipes
 print("Parsing Recipes")
 recipes_data = pandas.read_csv("data/solr/recipes.csv")
 recipes_data['Type'] = "recipe"
-recipes_data.rename({"Id": "RecipeId", "UserId": "AuthorId"}, axis = 1, inplace = True)
+recipes_data.rename({
+    "Id": "RecipeId", 
+    "UserId": "AuthorId", 
+    "DatePublished": "Date",
+    "RecipeCategory": "Category", 
+    "RecipeIngredientParts": "Ingredients",
+    "RecipeServings": "Servings",
+    "RecipeYield": "Yield",
+    "RecipeInstructions": "Instructions"
+}, axis = 1, inplace = True)
 recipes_data = recipes_data.merge(users_data, how='left', on='AuthorId')
 
 
 json_str = "["
 
-for i in range(0, 15000):
-    if i > 0: json_str = json_str + ","
+random.seed(1)
+indexes = sorted(random.sample(range(0, len(recipes_data)), 100))
 
-    recipe = recipes_data[i:i+1].to_dict('records')[0]
+for i in indexes:
+    recipe = recipes_data[i:i+1].replace({np.nan:None}).to_dict('records')[0]
+
+    if recipe["Images"] != None: recipe["Images"] = re.split(", ", recipe["Images"])
+    if recipe["Keywords"] != None: recipe["Keywords"] = re.split(", ", recipe["Keywords"])
+    if recipe["Ingredients"] != None: recipe["Ingredients"] = re.split(", ", recipe["Ingredients"])
+    if recipe["Instructions"] != None: recipe["Instructions"] = re.split(r"(?<=\.), ", recipe["Instructions"])
+
     reviews = reviews_data[reviews_data["RecipeId"] == recipe["RecipeId"]]
     
     if reviews.size > 0: 
         reviews = reviews.drop('RecipeId', axis = 1).to_dict('records')
         recipe["reviews"] = reviews
 
-    json_str = json_str + simplejson.dumps(recipe, ignore_nan = True)
+    json_str = json_str + simplejson.dumps(recipe, ignore_nan = True) + ","
 
-json_str = json_str + "]"
+json_str = json_str[:-1] + "]"
 
 
 ### Save JSON
