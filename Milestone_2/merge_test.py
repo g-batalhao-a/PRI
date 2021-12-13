Fixed average precision

Milestone_2/merge_test.py
@@ -1,50 +1,61 @@
+import numpy as np
 import pandas
-
-json_str = ""
-
-### Recipes
-
-file_path = "data/solr/recipes.csv"
-print("Opening file: " + file_path)
-
-data = pandas.read_csv(file_path) # Remove nrows for production
-data['Type'] = "recipe"
-data.rename({"Id": "RecipeId"}, axis = 1, inplace = True)
-
-json_data = data.head(1000).to_json(orient = "records")[:-1]
-
-json_str = json_str + json_data
-
-
-### Reviews
-
-file_path = "data/solr/reviews.csv"
-print("Opening file: " + file_path)
-
-data = pandas.read_csv(file_path) # Remove nrows for production
-data['Type'] = "review"
-data.drop('Id', axis = 1, inplace = True)
-
-json_data = data.head(1000).to_json(orient = "records")[1:-1]
-
-json_str = json_str + "," + json_data
-
+import simplejson
+import random
+import re
 
 ### Users
+print("Parsing Users")
+users_data = pandas.read_csv("data/solr/users.csv")
+users_data.rename({"Id": "AuthorId", "Name": "AuthorName"}, axis = 1, inplace = True)
 
-file_path = "data/solr/users.csv"
-print("Opening file: " + file_path)
+### Reviews
+print("Parsing Reviews")
+reviews_data = pandas.read_csv("data/solr/reviews.csv")
+reviews_data.rename({
+    "Id": "ReviewId", 
+    "UserId": "AuthorId",
+    "DateModified": "Date"
+}, axis = 1, inplace = True)
+reviews_data = reviews_data.merge(users_data, how='left', on='AuthorId')
+reviews_data["Review"] = "{" + reviews_data["Rating"].astype(str) + " stars, " + reviews_data["AuthorName"] + "} " + reviews_data["Review"]
+reviews_data['Reviews'] = reviews_data.groupby(['RecipeId'])['Review'].transform(lambda x : '., '.join(map(str, x)))
+reviews_data = reviews_data.drop(['ReviewId', 'Date', 'AuthorId', 'AuthorName', 'Review', 'Rating'], axis = 1).drop_duplicates() 
 
-data = pandas.read_csv(file_path) # Remove nrows for production
-data['Type'] = "user"
-data.rename({"Id": "UserId"}, axis = 1, inplace = True)
+### Recipes
+print("Parsing Recipes")
+recipes_data = pandas.read_csv("data/solr/recipes.csv")
+recipes_data.rename({
+    "Id": "RecipeId", 
+    "UserId": "AuthorId", 
+    "DatePublished": "Date",
+    "RecipeCategory": "Category", 
+    "RecipeIngredientParts": "Ingredients",
+    "RecipeServings": "Servings",
+    "RecipeYield": "Yield",
+    "RecipeInstructions": "Instructions"
+}, axis = 1, inplace = True)
+recipes_data = recipes_data.merge(users_data, how='left', on='AuthorId')
+recipes_data = recipes_data.merge(reviews_data, how='left', on='RecipeId')
 
-json_data = data.head(1000).to_json(orient = "records")[1:]
+jsonfile = open("solr/recipes_test.json", 'w+')
+jsonfile.write("[")
 
-json_str = json_str + "," + json_data
+random.seed(1)
+indexes = sorted(random.sample(range(0, len(recipes_data)), 100))
+
+for i in indexes:
+    recipe = recipes_data.iloc[[i]].replace({np.nan:None}).to_dict('records')[0]
+
+    if recipe["Images"] != None: recipe["Images"] = re.split(", ", recipe["Images"])
+    if recipe["Keywords"] != None: recipe["Keywords"] = re.split(", ", recipe["Keywords"])
+    if recipe["Ingredients"] != None: recipe["Ingredients"] = re.split(", ", recipe["Ingredients"])
+    if recipe["Instructions"] != None: recipe["Instructions"] = re.split(r"(?<=\.), ", recipe["Instructions"])
+    if recipe["Reviews"] != None: recipe["Reviews"] = re.split(r"\., ", recipe["Reviews"])
+
+    jsonfile.write(simplejson.dumps(recipe, ignore_nan = True) + ",")
+
+    if i % 1000 == 0: print(i)
 
 
-### Save JSON
-
-with open("solr/recipes_test.json", 'w+') as jsonfile:
-    jsonfile.write(json_str)
+jsonfile.write("]")