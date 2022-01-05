5d1552badc9ba629a7ac01265b266f15712fe182
antbz <antonio.bezerra@fe.up.pt>
2022-01-05 18:24:19 +0000

    LTR working

--- Dockerfile
+++ Dockerfile
@@ -6,6 +6,9 @@ COPY ${DB} /data/recipes.json
 
 COPY solr/schema.json /data/schema.json
 COPY solr/synonyms.txt /data/synonyms.txt
+COPY solr/ltr-plugin.xml /data/ltr-plugin.xml
+COPY solr/features.json /data/features.json
+COPY solr/ltr_model.json /data/ltr_model.json
 
 COPY startup.sh /scripts/startup.sh
 
--- startup.sh
+++ startup.sh
@@ -2,20 +2,28 @@
 
 precreate-core recipes
 
+cp /data/synonyms.txt /var/solr/data/recipes/conf
+
+sed -i $'/<\/config>/{e cat /data/ltr-plugin.xml\n}' /var/solr/data/recipes/conf/solrconfig.xml
+
 # Start Solr in background mode so we can use the API to upload the schema
 solr start -Dsolr.ltr.enabled=true
 
 sleep 10
-
-cp /data/synonyms.txt /var/solr/data/recipes/conf
 
 # Schema definition via API
 curl -X POST -H 'Content-type:application/json' \
     --data-binary @/data/schema.json \
     http://localhost:8983/solr/recipes/schema
 
+# Upload features for LTR
+curl -XPUT 'http://localhost:8983/solr/recipes/schema/feature-store' --data-binary "@/data/features.json" -H 'Content-type:application/json'
+
+# Upload model for LTR
+curl -XPUT 'http://localhost:8983/solr/recipes/schema/model-store' --data-binary "@/data/ltr_model.json" -H 'Content-type:application/json'
+
 # Populate collection
 bin/post -c recipes /data/recipes.json
 
 # Restart in foreground mode so we can access the interface
-solr restart -f
+solr restart -f -Dsolr.ltr.enabled=true
